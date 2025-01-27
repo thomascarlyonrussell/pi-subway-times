4c896b3fa9562d199b887a0fc34260986082d8bb
tom.russell <tom.russell@integralanalytics.com>
2025-01-26 21:21:05 -0500

    Fix typo in trip direction assignment for line 'F'

--- api/data_processing.py
+++ api/data_processing.py
@@ -55,7 +55,7 @@ def get_mta_data(stations, trip_directions):
                             trip['line'] = entity_trip.trip.route_id
                             trip['arrival_time'] = arrival_time.strftime('%H:%M')
                             trip['minutes_until_arrival'] = round((arrival_time.timestamp() - current_time) / 60)
-                            if trip.get('line')=='F': direction = 'Manhattan'
+                            if trip.get('line')=='F': direction = 'Manhatta'
                             elif trip.get('line')=='G': direction = 'Queens'
                             else: direction =='Adventure Time'
                             trip['direction'] = direction
