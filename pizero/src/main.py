3a963608d68288b872fe0f8df2b95d68dc2de4a3
tom.russell <tom.russell@integralanalytics.com>
2025-02-22 20:47:22 -0500

    adjust text positioning for arrival times and directions in display

--- pizero/src/main.py
+++ pizero/src/main.py
@@ -71,22 +71,22 @@ while True:
     color_value = int(TRIP_JSON[0].get('route_color', 'FFFFFF'), 16)
     color = get_clamped_color(color_value)
     graphics.DrawText(canvas, route_font, 0, 10, color, TRIP_JSON[0]["line"])
-    graphics.DrawText(canvas, font, 10, 10, color, TRIP_JSON[0]["direction"][:LINE_DIRECTION_MAX_LENGTH])
-    graphics.DrawText(canvas, font, 53, 10, color, str(TRIP_JSON[0]["minutes_until_arrival"]))
+    graphics.DrawText(canvas, font, 11, 9, color, TRIP_JSON[0]["direction"][:LINE_DIRECTION_MAX_LENGTH])
+    graphics.DrawText(canvas, font, 55, 9, color, str(TRIP_JSON[0]["minutes_until_arrival"]))
 
     # Second Row
     color_value = int(TRIP_JSON[1].get('route_color', 'FFFFFF'), 16)
     color = get_clamped_color(color_value)
     graphics.DrawText(canvas, route_font, 0, 20, color, TRIP_JSON[1]["line"])
-    graphics.DrawText(canvas, font, 10, 20, color, TRIP_JSON[1]["direction"][:LINE_DIRECTION_MAX_LENGTH])
-    graphics.DrawText(canvas, font, 53, 10, color, str(TRIP_JSON[1]["minutes_until_arrival"]))
+    graphics.DrawText(canvas, font, 11, 19, color, TRIP_JSON[1]["direction"][:LINE_DIRECTION_MAX_LENGTH])
+    graphics.DrawText(canvas, font, 55, 19, color, str(TRIP_JSON[1]["minutes_until_arrival"]))
 
     # Third Row
     color_value = int(TRIP_JSON[bottom_row_index].get('route_color', 'FFFFFF'), 16)
     color = get_clamped_color(color_value)
     graphics.DrawText(canvas, route_font, 0, 30, color, TRIP_JSON[bottom_row_index]["line"])
-    graphics.DrawText(canvas, font, 10, 30, color, TRIP_JSON[bottom_row_index]["direction"][:LINE_DIRECTION_MAX_LENGTH])
-    graphics.DrawText(canvas, font, 53, 30, color, str(TRIP_JSON[bottom_row_index]["minutes_until_arrival"]))
+    graphics.DrawText(canvas, font, 11, 29, color, TRIP_JSON[bottom_row_index]["direction"][:LINE_DIRECTION_MAX_LENGTH])
+    graphics.DrawText(canvas, font, 55, 29, color, str(TRIP_JSON[bottom_row_index]["minutes_until_arrival"]))
 
     elapsed_time = time.monotonic() - start_time
     pixels_on = int(((REFRESH_TIME_DELAY - elapsed_time) / REFRESH_TIME_DELAY) * LED_COLUMNS)
